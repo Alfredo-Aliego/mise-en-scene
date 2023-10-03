784e9dd426a13d7942aebf3bd39aa14bfb0ce838
manfredjoa <joamanfred@gmail.com>
2023-10-03 18:50:06 -0400

    add input ternary logic

--- app/components/nav/Nav.tsx
+++ app/components/nav/Nav.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Link from "next/link";
 import Search from "./Search";
-import { FC, useState } from "react";
+import { FC } from "react";
 
 type NavProps = {
   searchBar: boolean;
@@ -16,26 +16,32 @@ const Nav: FC<NavProps> = ({ searchBar, setSearchBar }) => {
       <main className="w-screen flex flex-col justify-between items-center p-4 h-20vh bg-black text-white relative">
         <header>Mise-En-Scène</header>
 
-        <nav className="flex w-screen justify-center z-10">
-          <section className="flex gap-5">
+        <section className="flex w-screen justify-center z-10">
+          <nav className="flex gap-5">
             <Link href="/">Home</Link>
             <Link href="/movies">Movies</Link>
+            <Link href="/director">Director</Link>
             <Link href="/genre">Genre</Link>
             <Link href="/year">Year</Link>
-          </section>
+          </nav>
 
           <Search handleSearch={handleSearch} />
-        </nav>
+        </section>
       </main>
 
-      {/* The translate transition works, but it takes up the space where it will transform to, even when searchBar is false. Currently using an alternative where there is no transition but the content underneath moves dynamically. */}
       <aside
-        className={`absolute top-20vh left-0 w-full bg-black h-10vh transition-transform duration-500 ${
+        className={`flex items-center absolute top-20vh left-0 w-full bg-black h-10vh transition-transform duration-500 ${
           searchBar
             ? "transform translate-y-0"
             : "transform translate-y-[-10vh]"
         }`}
-      ></aside>
+      >
+        <input
+          className={`w-screen px-4 transition-opacity outline-none ${
+            searchBar ? "delay-500 opacity-100" : "opacity-0 invisible"
+          }`}
+        ></input>
+      </aside>
     </>
   );
 };
