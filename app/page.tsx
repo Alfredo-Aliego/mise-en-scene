21c2f9aac3ff7480fb599e353a194902c3b6a497
nnmcgovern <48661257+nnmcgovern@users.noreply.github.com>
2023-10-03 17:06:40 -0400

    api functions: getMovies, getMovieById, getStills, getStillById tested and working

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,21 @@
+// test
+import { getMovies, getMovieById, getStills, getStillById } from "../api/api";
+
 export default function HomePage() {
+  // test
+  const movies = async () => {
+    let movies = await getMovies()
+    // console.log(movies)
+    let oneMovie = await getMovieById("tt0361862")
+    // console.log(oneMovie)
+    let stills = await getStills()
+    // console.log(stills)
+    let oneStill = await getStillById("1")
+    // console.log(oneStill)
+  }
+
+  movies()
+
   return (
     <main>
       HomePage
