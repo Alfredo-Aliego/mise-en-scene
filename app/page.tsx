d9575573f96f39c265e4f5e6b5ca466065ad5ba7
alantothe <104442054+alantothe@users.noreply.github.com>
2023-10-05 02:20:23 -0400

    install react-masonry & add masonry to home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 import { getMovies } from "../api/api.js";
 import { Fragment, useEffect, useState } from "react";
 import Link from "next/link";
+import Masonry from "react-masonry-css";
 
 type Movies = {
   stills: Still[];
@@ -28,29 +29,31 @@ export default function HomePage() {
   }
 
   return (
-    <main className="m-4 pt-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-      {movies.length > 0 ? (
-        movies.map((movie) => (
-          <Fragment key={movie.imdb_id}>
-            <div className="relative group overflow-hidden">
-              <img
-                src={movie.stills[11]?.image_url}
-                alt={movie.title}
-                className="transition-transform transform group-hover:scale-110 w-full h-auto"
-              />
-              <div className="overflow-hidden absolute inset-0 bg-black bg-opacity-40 group-hover:opacity-100 opacity-0 transition-opacity flex justify-center items-center">
-                <span className="text-white font-bold z-10">
-                  <Link href={`/${movie.title}`}>
-                    <h1>{movie.title}</h1>
-                  </Link>
-                </span>
+    <main className="m-4 pt-4 ">
+      <Masonry breakpointCols={4} className="flex -ml-8" columnClassName="pl-8">
+        {movies.length > 0 ? (
+          movies.map((movie) => (
+            <Fragment key={movie.imdb_id}>
+              <div className="relative group overflow-hidden bg-gray-400 mb-8">
+                <img
+                  src={movie.stills[11]?.image_url}
+                  alt={movie.title}
+                  className="transition-transform transform group-hover:scale-110 w-full h-auto"
+                />
+                <div className="overflow-hidden absolute inset-0 bg-black bg-opacity-40 group-hover:opacity-100 opacity-0 transition-opacity flex justify-center items-center">
+                  <span className="text-white font-bold z-10">
+                    <Link href={`/${movie.title}`}>
+                      <h1>{movie.title}</h1>
+                    </Link>
+                  </span>
+                </div>
               </div>
-            </div>
-          </Fragment>
-        ))
-      ) : (
-        <aside className="block mx-auto loading loading-bars loading-lg scale-150"></aside>
-      )}
+            </Fragment>
+          ))
+        ) : (
+          <aside className="block mx-auto loading loading-bars loading-lg scale-150"></aside>
+        )}
+      </Masonry>
     </main>
   );
 }
