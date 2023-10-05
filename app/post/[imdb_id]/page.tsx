efec379f663e9a9e7d1131a2c2029f1444c52948
alantothe <104442054+alantothe@users.noreply.github.com>
2023-10-05 03:56:11 -0400

    add api call to [imdb_id]

--- app/post/[imdb_id]/page.tsx
+++ app/post/[imdb_id]/page.tsx
@@ -1,3 +1,37 @@
+"use client";
+import { useState, useEffect } from "react";
+import { getMovieById } from "@/api/api";
+
+type Movie = {
+  imdb_id: string;
+  title: string;
+  date_released: string;
+  genre: string;
+  rating: string;
+  director: string;
+  country: string;
+  imdb_rating: string;
+  stills: Still[];
+};
+
+type Still = {
+  id: number;
+  image_url: string;
+  imdb_id: string;
+};
+
 export default function Page({ params }: { params: { imdb_id: string } }) {
+  const [movie, setMovie] = useState<Movie[]>([]);
+
+  useEffect(() => {
+    fetchMovie();
+  }, []);
+
+  const fetchMovie = async () => {
+    let fetchedMovie: Movie[] = await getMovieById(params.imdb_id);
+    setMovie(fetchedMovie);
+    console.log(fetchedMovie);
+  };
+
   return <div>My Post:{params.imdb_id}</div>;
 }
