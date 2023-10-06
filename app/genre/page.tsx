1bb0eea68cb19040e3661975bac0eba9984c4168
manfredjoa <joamanfred@gmail.com>
2023-10-06 01:41:08 -0400

    create Genre component, imported into GenrePage

--- app/genre/page.tsx
+++ app/genre/page.tsx
@@ -1,54 +1,7 @@
-"use client";
-import React, { useState, useEffect } from "react";
-import { getGenresOnly } from "@/api/api";
-import Link from "next/link";
-import LoadingBars from "../components/loading/LoadingBars";
-
-type Genre = {
-  genre: string;
-};
-
-type UniqueGenre = string[];
+import Genre from "../components/genre/Genre";
 
 const GenrePage = () => {
-  const [genres, setGenres] = useState<Genre[]>([]);
-  const [uniqueGenres, setUniqueGenres] = useState<UniqueGenre>([]);
-  const allGenres: string[] = [];
-
-  useEffect(() => {
-    fetchGenres();
-  }, []);
-
-  useEffect(() => {
-    getUniqueGenres();
-  }, [genres]);
-
-  const fetchGenres = async () => {
-    const fetchedGenres = await getGenresOnly();
-    setGenres(fetchedGenres);
-  };
-
-  const getUniqueGenres = () => {
-    genres.forEach((genre) => {
-      const splitGenres = genre.genre.split(", ");
-      allGenres.push(...splitGenres);
-    });
-    setUniqueGenres(Array.from(new Set(allGenres.sort())));
-  };
-
-  return (
-    <main className="m-4 pt-4">
-      {uniqueGenres.length > 0 ? (
-        uniqueGenres.map((genre, index) => (
-          <Link key={index} href={`/genre/${genre}`} className="block w-max">
-            {genre}
-          </Link>
-        ))
-      ) : (
-        <LoadingBars />
-      )}
-    </main>
-  );
+  return <Genre />;
 };
 
 export default GenrePage;
