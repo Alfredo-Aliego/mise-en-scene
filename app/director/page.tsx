6027793ae4d161ad86469160927d7c2ab85095a8
manfredjoa <joamanfred@gmail.com>
2023-10-06 01:42:23 -0400

    create Director component, imported into DirectorPage

--- app/director/page.tsx
+++ app/director/page.tsx
@@ -1,58 +1,8 @@
-"use client";
-import React, { useState, useEffect } from "react";
-import { getDirectorsOnly } from "@/api/api";
-import Link from "next/link";
-import LoadingBars from "../components/loading/LoadingBars";
+import React from "react";
+import Director from "../components/director/Director";
 
-type Director = {
-  director: string;
+const page = () => {
+  return <Director />;
 };
 
-type UniqueDirector = string[];
-
-const DirectorPage = () => {
-  const [directors, setDirectors] = useState<Director[]>([]);
-  const [uniqueDirectors, setUniqueDirectors] = useState<UniqueDirector>([]);
-  const allDirectors: string[] = [];
-
-  useEffect(() => {
-    fetchDirectors();
-  }, []);
-
-  useEffect(() => {
-    getUniqueDirectors();
-  }, [directors]);
-
-  const fetchDirectors = async () => {
-    const fetchedDirectors = await getDirectorsOnly();
-    setDirectors(fetchedDirectors);
-  };
-
-  const getUniqueDirectors = () => {
-    directors.forEach((director) => {
-      const splitDirectors = director.director.split(", ");
-      allDirectors.push(...splitDirectors);
-    });
-    setUniqueDirectors(Array.from(new Set(allDirectors.sort())));
-  };
-
-  return (
-    <main className="m-4 pt-4">
-      {uniqueDirectors.length > 0 ? (
-        uniqueDirectors.map((director, index) => (
-          <Link
-            key={index}
-            href={`/director/${director}`}
-            className="block w-max"
-          >
-            {director}
-          </Link>
-        ))
-      ) : (
-        <LoadingBars />
-      )}
-    </main>
-  );
-};
-
-export default DirectorPage;
+export default page;
