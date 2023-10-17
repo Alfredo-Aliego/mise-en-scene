0ce044f86d7ab07057cdb30c0ba565dd29777d7d
manfredjoa <joamanfred@gmail.com>
2023-10-17 12:51:24 -0400

    add getCountriesOnly fetch and convert Country component to SSR

--- app/components/country/Country.tsx
+++ app/components/country/Country.tsx
@@ -1,48 +1,33 @@
-"use client";
-import React, { useState, useEffect } from "react";
-import { useRouter } from "next/navigation";
-import { getCountriesOnly } from "@/api/api";
+import Link from "next/link";
+import { getCountriesOnly } from "@/api/lib/getCountriesOnly";
 import LoadingBars from "../loading/LoadingBars";
 
-const Country = () => {
-  const router = useRouter();
+const Country = async () => {
+  const countries: Promise<Country[]> = getCountriesOnly();
+  let uniqueCountries = await countries;
+  let allCountries: string[] = [];
 
-  const [countries, setCountries] = useState<Country[]>([]);
-  const [uniqueCountries, setUniqueCountries] = useState<UniqueCountry>([]);
-  const allCountries: string[] = [];
+  console.log(uniqueCountries);
+  uniqueCountries.forEach((country) => {
+    const splitCountries = country.country.split(", ");
+    allCountries = allCountries.concat(splitCountries);
+  });
 
-  useEffect(() => {
-    fetchCountries();
-  }, []);
-
-  useEffect(() => {
-    getUniqueCountries();
-  }, [countries]);
-
-  const fetchCountries = async () => {
-    const fetchedCountries = await getCountriesOnly();
-    setCountries(fetchedCountries);
-  };
-
-  const getUniqueCountries = () => {
-    countries.forEach((country) => {
-      const splitCountries = country.country.split(", ");
-      allCountries.push(...splitCountries);
-    });
-    setUniqueCountries(Array.from(new Set(allCountries.sort())));
-  };
+  uniqueCountries = Array.from(new Set(allCountries.sort())).map((country) => ({
+    country,
+  }));
 
   return (
     <main className="flex justify-center flex-wrap gap-8 m-4 pt-8">
       {uniqueCountries.length > 0 ? (
         uniqueCountries.map((country, index) => (
-          <article
+          <Link
             key={index}
+            href={`/country/${country.country}`}
             className="w-[26vw] h-[13vw] flex justify-center items-center text-center bg-secondary cursor-pointer hover:opacity-50 text-4xl border border-current shadow-lg shadow-current"
-            onClick={() => router.push(`/country/${country}`)}
           >
-            {`[ ${country} ]`}
-          </article>
+            {`[ ${country.country} ]`}
+          </Link>
         ))
       ) : (
         <LoadingBars />
