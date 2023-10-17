e365be9adc39f4e6c076fd1fbb0e69de866e3422
alantothe <104442054+alantothe@users.noreply.github.com>
2023-10-16 23:40:29 -0400

    fix run build error

--- app/components/director/Director.tsx
+++ app/components/director/Director.tsx
@@ -12,8 +12,8 @@ const Director = async () => {
   return (
     <main className="flex justify-center flex-wrap gap-8 m-4 pt-8">
       {directors.length > 0 ? (
-        directors.map((director) => (
-          <Link href={`/director/${director.director}`}>
+        directors.map((director, index) => (
+          <Link key={index} href={`/director/${director.director}`}>
             <article className="w-[26vw] h-[13vw] flex justify-center items-center text-center bg-secondary cursor-pointer hover:opacity-50 text-4xl border border-current shadow-lg shadow-current">
               {`[ ${director.director} ]`}
             </article>
